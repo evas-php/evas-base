ccd486a0e0356fb67d679f4d383e5d0854b28eca
Egor Vasyakin <evasyakin@mail.ru>
2020-01-09 13:45:56 +0700

    Добавлен Хелпер для директории запуска

---
+++ src/RunDirHelper.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package evas-php/evas-base
+ */
+namespace Evas\Base;
+
+/**
+ * Хелпер для директории запуска.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class RunDirHelper
+{
+    /**
+     * Вспомогательный метод для удаления слеша в конце директории.
+     * @param string путь
+     * @return string
+     */
+    public static function addEndDirSlash(string $dir): string
+    {
+        return str_replace('\\', '/', realpath($dir)) . '/';
+    }
+
+    /**
+     * Получение директории запуска приложения.
+     * @return string
+     */
+    public static function getDir(): string
+    {
+        static $dir = null;
+        if (null === $dir) {
+            $dir = static::addEndDirSlash(dirname($_SERVER['SCRIPT_FILENAME']) . '/');
+        }
+        return $dir;
+    }
+}
