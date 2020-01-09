ddf130f9534f1b89f233265da8b6c704e1fd7451
Egor Vasyakin <evasyakin@mail.ru>
2020-01-09 13:45:42 +0700

    Создан PhpHelper, добавлены вспомогательные методы для массивов

---
+++ src/PhpHelper.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package evas-php/evas-base
+ */
+namespace Evas\Base;
+
+/**
+ * Кастомизация базового php.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class PhpHelper
+{
+    /**
+     * Проверка массива на ассоциативность.
+     * @return bool
+     */
+    public static function isAssoc($arr): bool
+    {
+        return is_array($arr) && array_keys($arr) !== range(0, count($arr) - 1);
+    }
+
+    /**
+     * Проверка на не ассоциативный массив.
+     * @return bool
+     */
+    public static function isNotAssoc($arr): bool
+    {
+        return !static::isAssoc($arr);
+    }
+
+    /**
+     * Проверка на нумерованный массив.
+     * @return bool
+     */
+    public static function isNumericArray($arr): bool
+    {
+        return is_array($arr) && !static::isAssoc($arr);
+    }
+}
