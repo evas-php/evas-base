a0909b5287b2a598b1c0efa9a38a59e0b1129e52
Egor Vasyakin <evasyakin@mail.ru>
2020-01-09 13:44:54 +0700

    Добавлено исключение ненайденного свойства

---
+++ src/Exception/UndefinedPropertyException.php
@@ -0,0 +1,13 @@
+<?php
+namespace Evas\Base\Exception;
+
+use Evas\Base\Exception as BaseException;
+
+/**
+ * Исключение не найдено свойство.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class UndefinedPropertyException extends BaseException
+{
+}
