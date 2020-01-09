2362cbd6ad8cdb6235382c9ed78ef0b7cd746060
Egor Vasyakin <evasyakin@mail.ru>
2020-01-09 13:49:02 +0700

    Добавлен Трейт поддежки методов директории приложения.

---
+++ src/AppDirTrait.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @package evas-php/evas-base
+ */
+namespace Evas\Base;
+
+use Evas\Base\RunDirHelper;
+
+/**
+ * Трейт поддежки методов директории приложения.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+trait AppDirTrait
+{
+    /**
+     * @var string директория приложения
+     */
+    protected $dir;
+
+    /**
+     * Установка базовой директории приложения.
+     * @param string
+     * @return self
+     */
+    public static function setDir(string $dir)
+    {
+        return static::instanceSet('dir', RunDirHelper::addEndDirSlash($dir));
+    }
+
+    /**
+     * Получение базовой директории приложения.
+     * @return string
+     */
+    public static function getDir(): string
+    {
+        if (!static::instanceHas('dir')) {
+            static::setDir(static::getRunDir());
+        }
+        return static::instanceGet('dir');
+    }
+
+    /**
+     * Получение директории запуска приложения.
+     * @return string
+     */
+    public static function getRunDir(): string
+    {
+        return RunDirHelper::getDir();
+    }
+}
