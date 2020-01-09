174e97497572c3c14fae512a832b1f57eee9a198
Egor Vasyakin <evasyakin@mail.ru>
2020-01-09 13:46:43 +0700

    Добавлен Хелпер трейт и класс для сборки сообщения из переменных класса с помощью подстановок (MessageVarReplacer)

---
+++ src/MessageVarsReplacer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package evas-php/evas-base
+ */
+namespace Evas\Base;
+
+use Evas\Base\MessageReplacerTrait;
+
+/**
+ * Класс-хепер для подстановок значений переменных в сообщения.
+ * на место :var подставляет значение переменной var
+ * на место <текст :var> подставляет текст + переменную var, только при наличии значения var
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class MessageVarsReplacer
+{
+    use MessageVarsReplacerTrait;
+}
---
+++ src/MessageVarsReplacerTrait.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @package evas-php/evas-base
+ */
+namespace Evas\Base;
+
+/**
+ * Трейт-хелпер для подстановок значений переменных в сообщения.
+ * на место :var подставляет значение переменной var
+ * на место <текст :var> подставляет текст + переменную var, только при наличии значения var
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+trait MessageVarsReplacerTrait
+{
+    /**
+     * Подстановка значения переменных в сообщение.
+     * @param string сообщение
+     * @param array|null значения для подстановки
+     * @return string ообщение с вставленными переменными
+     */
+    public function setMessageVars(string $message, array $vars = null): string
+    {
+        if (null === $vars) return $vars = @get_object_vars($this) ?? null;
+        if (null === $vars) return $message;
+        $message = preg_replace_callback(
+            '/<(?<before>[^<]*):(?<key>[a-zA-Z]*)(?<after>[^<]*)>|:(?<key2>[a-zA-Z]*)/', 
+            function ($matches) {
+                extract($matches);
+                if (empty($key)) $key = $key2;
+                $value = $vars[$key] ?? null;
+                if (empty($value)) {
+                    if (empty(static::$message_aliases_keys)) return '';
+                    $alias = static::$message_aliases_keys[$key] ?? null;
+                    if (empty($alias)) return '';
+                    $value = $vars[$alias] ?? null;
+                    if (empty($value)) return '';
+                }
+                return $before . $value . $after;
+            }, 
+            $message);
+        return $message;
+    }
+}
