8273aa208dd97bfa7c984c19f107b80626841659
Egor Vasyakin <evasyakin@mail.ru>
2020-01-09 13:44:27 +0700

    Добавлен базовый класс исключения

---
+++ src/Exception.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package evsa-php/evas-base
+ */
+namespace Evas\Base;
+
+use \Exception as PhpException;
+
+/**
+ * Базовый класс исключения.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class Exception extends PhpException
+{
+}
