f20155d92f59a6994badbcccdf6aecb3aed17a3a
Egor Vasyakin <evasyakin@mail.ru>
2020-02-01 23:11:06 +0700

    Create FileNotFoundException.php

---
+++ src/Exception/FileNotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+namespace Evas\Base\Exception;
+
+use Evas\Base\BaseException;
+
+/**
+ * Исключение не найден файл.
+ * @author Egor Vasyakin <e.vasyakin@itevas.ru>
+ * @since 1.0
+ */
+class FileNotFoundException extends BaseException
+{}
