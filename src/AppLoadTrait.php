d47d79ecaf3d1910d8aa92b9e866dd6b797179be
Egor Vasyakin <e.vasyakin@itevas.ru>
2020-07-31 13:22:04 +0300

    Update AppLoadTrait.php

--- src/AppLoadTrait.php
+++ src/AppLoadTrait.php
@@ -5,6 +5,7 @@
 namespace Evas\Base;
 
 use Evas\Base\Exception\FileNotFoundException;
+use Evas\Base\LoadTrait;
 
 /**
  * Трейт расширения приложения подгрузкой файлов.
@@ -14,6 +15,11 @@ use Evas\Base\Exception\FileNotFoundException;
 trait AppLoadTrait
 {
     /**
+     * Подключаем трейт подгрузки файлов.
+     */
+    use LoadTrait;
+
+    /**
      * Получение полного имени файла относительно корня приложения.
      * @param string относительное имя файла
      * @return string олное имя файла
@@ -21,46 +27,6 @@ trait AppLoadTrait
     public static function filename(string $filename): string
     {
         return static::dir() . $filename;
-    }
-
-    /**
-     * Подгрузить содержимое файла относительно корня приложения или по абсолютному пути.
-     * @param string имя файла
-     * @param array аргументы для загружаемого файла
-     * @param object|null контекст
-     * @throws FileNotFoundException
-     * @return mixed|null возвращаемый результат файла
-     */
-    public static function load(string $filename, array $args = [], object &$context = null)
-    {
-        static::throwIfNotCanLoad($filename);
-        $load = function () use ($filename, $args) {
-            extract($args);
-            return include $filename;
-        };
-        if ($context) $load = $load->bindTo($context);
-        return $load();
-    }
-
-    /**
-     * Проверка на возможность загрузить файл.
-     * @param string имя файла
-     * @return bool удалось ли прочитать файл
-     */
-    public static function canLoad(string $filename): bool
-    {
-        return is_readable($filename) && is_file($filename) ? true : false;
-    }
-
-    /**
-     * Выбросить исключение, если не удается загрузить файл.
-     * @throws FileNotFoundException
-     */
-    public static function throwIfNotCanLoad(string $filename)
-    {
-        if (!static::canLoad($filename)) {
-            throw new FileNotFoundException("File \"$filename\" not found");
-        }
     }
 
     /**
