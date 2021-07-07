1f0cd2c44eca381b442a5c7a16b8a7914a00c5b8
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-07-07 09:34:38 +0300

    Create BaseException.php

---
+++ src/Exceptions/BaseException.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Класс исключений базового модуля.
+ * @package evas-php\evas-base
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Base\Exceptions;
+
+class BaseException extends \Exception
+{}
