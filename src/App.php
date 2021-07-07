79da54a94cc2856091f5d2d6572d70ee6fe1d55c
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-07-07 09:35:21 +0300

    Create App.php

---
+++ src/App.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Класс приложения.
+ * @package evas-php\evas-base
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Base;
+
+use Evas\Base\Traits\AppDiTrait;
+use Evas\Base\Traits\AppDirTrait;
+
+class App
+{
+    use AppDirTrait;
+    use AppDiTrait;
+
+    /** @var static инстанс приложения */
+    public static $instance;
+
+    /**
+     * Получение инстанса приложения.
+     * @return static
+     */
+    public static function instance()
+    {
+        if (!static::$instance) {
+            static::$instance = new static;
+        }
+        return static::$instance;
+    }
+
+    /**
+     * Установка инстанса приложения.
+     * @param self инстанс приложения
+     */
+    public static function setInstance(App $instance)
+    {
+        static::$instance = &$instance;
+    }
+}
