f328c0f44f0ff582d0346aae71c344dcfee58e6d
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-07-07 09:35:34 +0300

    Create IncludeTrait.php

---
+++ src/Traits/IncludeTrait.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Трейт загрузки файлов.
+ * @package evas-php\evas-base
+ * @author Egor Vasyakin <egor@evas-php.com>
+ */
+namespace Evas\Base\Traits;
+
+use Evas\Base\App;
+use Evas\Base\Exceptions\FileNotFoundException;
+
+trait IncludeTrait
+{
+    /**
+     * Загрузка файла.
+     * @param string путь файла
+     * @param array аргументы для загружаемого файла
+     * @param object|null контекст файла
+     * @return mixed|null возвращаемый результат файла
+     * @throws FileNotFoundException
+     */
+    public function include(string $filename, array $args = null, object &$context = null)
+    {
+        $filename = App::resolveByApp($filename);
+        $this->throwIfNotCanInclude($filename);
+        $load = function () use ($filename, $args) {
+            if (!empty($args)) extract($args);
+            return include $filename;
+        };
+        if ($context) $load = $load->bindTo($context);
+        return $load();
+    }
+
+    /**
+     * Проверка наличия файла.
+     * @param string путь файла
+     * @return bool найден ли файл
+     */
+    public function canInclude(string $filename): bool
+    {
+        $filename = App::resolveByApp($filename);
+        return is_readable($filename) && is_file($filename);
+    }
+
+    /**
+     * Выбросить исключение, если файл не найден.
+     * @param string путь файла
+     * @throws FileNotFoundException
+     */
+    public function throwIfNotCanInclude(string $filename)
+    {
+        if (!$this->canInclude($filename)) {
+            throw new FileNotFoundException($filename);
+        }
+    }
+}
