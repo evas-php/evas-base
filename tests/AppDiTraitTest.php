2175940408cd383819072524ab22e1b4e9b604c8
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-07-07 09:36:38 +0300

    Create AppDiTraitTest.php

---
+++ tests/AppDiTraitTest.php
@@ -0,0 +1,28 @@
+<?php
+use Evas\Base\tests;
+
+use Codeception\Util\Autoload;
+use Evas\Base\App;
+use Evas\Di\Container;
+
+Autoload::addNamespace('Evas\\Base', 'vendor/evas-php/evas-base/src');
+Autoload::addNamespace('Evas\\Di', 'vendor/evas-php/evas-di/src');
+
+class AppDiTraitTest extends \Codeception\Test\Unit
+{
+    public function testManualSetDi()
+    {
+        $di = new Container;
+        $di->set('test', 'testManualSetDi');
+        App::setDi($di);
+        $this->assertTrue(App::has('test'));
+        $this->assertEquals('testManualSetDi', App::get('test'));
+    }
+
+    public function testAutoSetDi()
+    {
+        App::set('test', 'testAutoSetDi');
+        $this->assertTrue(App::has('test'));
+        $this->assertEquals('testAutoSetDi', App::get('test'));
+    }
+}
